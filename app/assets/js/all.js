c2ede457a23ee5a0e49666f8e5b3c62a77c2dc3f
zplusz <washida0209@yahoo.com.tw>
2020-07-04 15:39:17 +0800

    f

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,24 @@
-$(document).ready(() => {
-  console.log('Hello Bootstrap4');
+$(document).ready(function () {
+  $('.hamburger').click(function (e) { 
+      
+      $('.hamburger').toggleClass('active');
+      $('.navbar').toggleClass('open');
+  });
+  $('.optical').click(function (e) { 
+    localStorage.setItem("css",".opticalline");
+    localStorage.setItem("display","display");
+    localStorage.setItem("block","block");
+  });
+  $('.sunglass').click(function (e) { 
+    localStorage.setItem("css",".sunglassline");
+    localStorage.setItem("display","display");
+    localStorage.setItem("block","block");
+  });
+  $('.functional').click(function (e) { 
+    localStorage.setItem("css",".functionalline");
+    localStorage.setItem("display","display");
+    localStorage.setItem("block","block");
+  });
+  $(localStorage.getItem("css")).css(localStorage.getItem("display"),localStorage.getItem("block"));
+  localStorage.clear();
 });
